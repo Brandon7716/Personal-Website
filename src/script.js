10d38571e87d4d53dbf4e11827be09faaa29ba57
Brandon7716 <brandon.emmerich.7716@gmail.com>
2025-05-04 23:15:44 -0400

    Add last update script.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
 console.log("Hello!");
+let lastUpdateText = document.querySelector("#last-update");
+fetch(
+  "https://api.github.com/repos/Brandon7716/Personal-Website/branches/master"
+)
+  .then((res) => {
+    return res.json();
+  })
+  .then((res) => {
+    let date = res.commit.commit.author.date;
+    lastUpdateText.textContent = new Date(date).toLocaleString();
+    console.log(date);
+  });
